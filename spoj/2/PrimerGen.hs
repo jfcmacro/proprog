886eee230fc67f3d417e62d0e38303e277260c17
Juan Francisco Cardona Mc'Cormick <jfcmacroeafit@gmail.com>
2013-08-17 16:14:30 -0500

    Some things has been changed but it is not yet working

--- spoj/2/PrimerGen.hs
+++ spoj/2/PrimerGen.hs
@@ -16,34 +16,60 @@ addRelPrime n = do
 computePrimesFrom :: Int -> Int -> StatePrim ()
 computePrimesFrom m n
   | m >= n      = return $ ()
-  | from < n    = do
+  | m < n    = do
     addRelPrime m
-    computePrimesFrom (m + 1) to
+    computePrimesFrom (m + 1) n
 
--- 'testLimits'
---
-testLimits :: Int -> Int -> StatePrim (Int,Int,Int)
-testLimits m n = do
-  prim <- get
-  let (i,m',n') = if null prim 
-                  then (2,m,n) 
-                  else let lp = last prim
-                       in if lp <= m
-                          then (lp,m,n)
-                          else if lp <= n
-                               then (lp,lp,n)
-                               else (lp,lp,lp)
-  return $ (i,m',n')
- 
+-- -- 'testLimits'
+-- --
+-- testLimits :: Int -> Int -> StatePrim
+-- testLimits m n = do
+--   prim <- get
+--   = if null prim
+--                   then (2,m,n)
+--                   else let lp = last prim
+--                        in if lp <= m
+--                           then (lp,m,n)
+--                           else if lp <= n
+--                                then (lp,lp,n)
+--                                else (lp,lp,lp)
+--   return $ (i,m',n')
+
 printPrimes :: Int -> Int -> StatePrim ()
 printPrimes m n  = do
-  (i,m',n') <- testLimits m n
-  computePrimesFrom i m'
-  prim'  <- get
-  computePrimesFrom m' n'
-  prim'' <- get
-  let result = prim'' \\ prim'
-  lift $ mapM_ print result >> putStrLn ""
+  prim <- get
+  if null prim
+     then do
+       computePrimesFrom 2 m
+       prim' <- get
+       computePrimesFrom m n
+       prim'' <- get
+       let result = prim'' \\ prim'
+       lift $ mapM_ print result >> putStrLn ""
+     else 
+       let lp = last prim
+       in if lp <= m
+          then do 
+            computePrimesFrom lp m
+            prim' <- get
+            computePrimesFrom m n
+            prim'' <- get
+            let result = prim'' \\ prim'
+            lift $ mapM_ print result >> putStrLn ""
+          else if lp <= n
+               then do
+                 r1 <- get
+                 let prim' = filter (<m) r1
+                 computePrimesFrom lp n
+                 prim'' <- get
+                 let result = prim'' \\ prim'
+                 lift $ mapM_ print result >> putStrLn ""
+               else do
+                 r1 <- get
+                 let prim'  = filter (<m) r1
+                     prim'' = filter (<n) r1
+                     result = prim'' \\ prim'
+                 lift $ mapM_ print result >> putStrLn ""
 
 evalPrintPrimes :: Int -> Int -> [Int] -> IO ((),[Int])
 evalPrintPrimes m n st = runStateT (printPrimes m n) st
@@ -53,8 +79,8 @@ process 1 st = return $ ()
 process n st = do
   l <- getLine
   let [m,n] = map read $ words l
-  (_,st) evalPrintPrimes m n st
-  process (n - 1)
+  (_,st') <- evalPrintPrimes m n st
+  process (n - 1) st'
 
 main :: IO ()
 main = do
